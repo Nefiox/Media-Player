5ecb458ba876ed95572963e33c41e4399142d7b8
Juan Jiménez <nefioxdev@gmail.com>
2021-10-21 11:34:50 -0500

    Including TypeScript

--- assets/sw.js
+++ assets/sw.js
@@ -26,7 +26,7 @@ async function precache() {
     // "/assets/index.js",
     // "/assets/MediaPlayer.js",
     // "/assets/plugins/AutoPlay.js",
-    // "/assets/plugins/AutoPause.js",
+    // "/assets/plugins/AutoPause.ts",
     // "/assets/index.css",
     // "/assets/Prologue.mp4",
   ]);
