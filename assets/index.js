bfebaf11bd35b3fba8e3fedd11152ffd9b4ebfd1
Juan Jiménez <nefioxdev@gmail.com>
2021-10-20 22:11:06 -0500

    Service Worker

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,7 @@ button.onclick = () => player.togglePlay();
 
 const mute = document.querySelector(".mute");
 mute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').catch(error => console.log(error.message));
+}
