017628eaef9c98a88ff3fee6a19a2448f5bef68a
Juan Jiménez <nefioxdev@gmail.com>
2021-10-22 01:06:49 -0500

    Finished

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from "./MediaPlayer";
-import AutoPlay from "./plugins/AutoPlay";
-import AutoPause from "./plugins/AutoPause";
-import Ads from "./plugins/Ads";
+import MediaPlayer from "@nefiox/mediaplayer";
+import AutoPlay from "@nefiox/mediaplayer/lib/plugins/AutoPlay";
+import AutoPause from "@nefiox/mediaplayer/lib/plugins/AutoPause";
+import Ads from "@nefiox/mediaplayer/lib/plugins/Ads";
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({
